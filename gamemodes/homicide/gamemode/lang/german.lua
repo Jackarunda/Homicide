4a13570a5ed8e7fabbc3fc6add1f5ae03cd144c4
Doresun <80627854+Doresun@users.noreply.github.com>
2022-09-15 03:17:57 +0300

    Space unremoval
    Also moved the resource folder to root for easier everything

--- gamemodes/homicide/gamemode/lang/german.lua
+++ gamemodes/homicide/gamemode/lang/german.lua
@@ -1,108 +1,108 @@
-pt.bystander="Unschuldiger"
-pt.murderer="Mörder"
-pt.spectator="Zuschauer"
+pt.bystander = "Unschuldiger"
+pt.murderer = "Mörder"
+pt.spectator = "Zuschauer"
 
-pt.knife="Messer"
-pt.magnum="Magnum"
-pt.gun="Waffe"
-pt.hands="Hände"
-
-
-pt.teamSpectators="Zuschauer"
-pt.teamPlayers="Spieler"
-pt.teamAss="Kuchen"
-
-pt.killedTeamKill="{player} hat eine unschuldige Person getötet"
-pt.killedMurderer="{player} hat den Mörder getötet"
-
-pt.murdererDeathUnknown="Der Mörder starb unter mysteriösen Umständen"
-
-pt.changeTeam="{player} wechselte zum Team {team}"
-pt.teamMoved="{player} wurde in das Team {team} verschoben"
-pt.teamMovedAFK="{player} wurde in das Team {team} verschoben, weil er AFK war"
-
-pt.spectateFailed="Du kannst nur zuschauen, wenn du tot bist."
-
-pt.murdererDisconnect="Der Mörder zog sich zurück (ragequit)"
-pt.murdererDisconnectKnown="Der Mörder hat sich zurückgezogen, es war {murderer}"
-
-pt.winBystanders="Die Unschuldigen gewinnen!"
-pt.winBystandersMurdererWas=" Der Mörder war {murderer}"
-pt.winMurderer="Der Mörder gewinnt!"
-pt.winMurdererMurdererWas=" Es war {murderer}"
-
-pt.minimumPlayers="Es sind nicht genug Spieler vorhanden, um die Runde zu starten!"
-pt.roundStarted="Eine neue Runde wurde gestartet"
+pt.knife = "Messer"
+pt.magnum = "Magnum"
+pt.gun = "Waffe"
+pt.hands = "Hände"
 
 
-pt.adminMurdererSelect="{player} wird der nächste Mörder sein"
-pt.adminMurdererForce="Als Mörder für die nächste Runde bestimmen"
-pt.adminSpectate="Zuschauen"
-pt.adminMoveToSpectate="Zu {spectate} verschieben"
+pt.teamSpectators = "Zuschauer"
+pt.teamPlayers = "Spieler"
+pt.teamAss = "Kuchen"
 
-pt.mapChange="Wechsle zu Map: {map}"
+pt.killedTeamKill = "{player} hat eine unschuldige Person getötet"
+pt.killedMurderer = "{player} hat den Mörder getötet"
 
-pt.scoreboard="Scoreboard"
-pt.scoreboardName="Name"
-pt.scoreboardPing="Ping"
-pt.scoreboardBystanderName="Name als Unschuldiger"
-pt.scoreboardStatus="Status"
-pt.scoreboardChance="Wahrscheinlichkeit"
-pt.scoreboardRefresh="Aktualisieren"
+pt.murdererDeathUnknown = "Der Mörder starb unter mysteriösen Umständen"
 
-pt.scoreboardJoinTeam="Beitreten"
+pt.changeTeam = "{player} wechselte zum Team {team}"
+pt.teamMoved = "{player} wurde in das Team {team} verschoben"
+pt.teamMovedAFK = "{player} wurde in das Team {team} verschoben, weil er AFK war"
 
-pt.scoreboardActionAdmin="Ist ein Admin"
-pt.scoreboardActionMute="Stumm schalten"
-pt.scoreboardActionUnmute="Stumm schalten deaktivieren"
+pt.spectateFailed = "Du kannst nur zuschauen, wenn du tot bist."
 
-pt.endroundMurdererQuit="Die Unschuldigen gewinnen! Der Mörder hat sich zurückgezogen"
-pt.endroundBystandersWin="Die Unschuldigen gewinnen!"
-pt.endroundMurdererWins="Der Mörder gewinnt!"
-pt.endroundMurdererWas="Der Mörder war {murderer}"
+pt.murdererDisconnect = "Der Mörder zog sich zurück (ragequit)"
+pt.murdererDisconnectKnown = "Der Mörder hat sich zurückgezogen, es war {murderer}"
 
-pt.endroundLootCollected="Gegenstände eingesammelt"
+pt.winBystanders = "Die Unschuldigen gewinnen!"
+pt.winBystandersMurdererWas = " Der Mörder war {murderer}"
+pt.winMurderer = "Der Mörder gewinnt!"
+pt.winMurdererMurdererWas = " Es war {murderer}"
 
-pt.adminPanel="Admin-Menü"
-
-pt.spectating="Zuschauen"
-
-pt.adMelonbomberWhy="Probier doch mal "
-pt.adMelonbomberBy=" ein Spielmodus von dem Entwickler von Murder!"
-
-pt.voiceHelp="Hilfe"
-pt.voiceHelpDescription="Schrei nach Hilfe"
-pt.voiceFunny="Witz"
-pt.voiceFunnyDescription="Ein kleiner Scherz"
-pt.voiceScream="Schrei"
-pt.voiceScreamDescription="Wie ein kleines Mädchen"
-pt.voiceMorose="Mürrisch"
-pt.voiceMoroseDescription="Fühle die Trauer"
+pt.minimumPlayers = "Es sind nicht genug Spieler vorhanden, um die Runde zu starten!"
+pt.roundStarted = "Eine neue Runde wurde gestartet"
 
 
-pt.startHelpBystanderTitle="Du bist eine unschuldige Person"
-pt.startHelpBystander={
+pt.adminMurdererSelect = "{player} wird der nächste Mörder sein"
+pt.adminMurdererForce = "Als Mörder für die nächste Runde bestimmen"
+pt.adminSpectate = "Zuschauen"
+pt.adminMoveToSpectate = "Zu {spectate} verschieben"
+
+pt.mapChange = "Wechsle zu Map: {map}"
+
+pt.scoreboard = "Scoreboard"
+pt.scoreboardName = "Name"
+pt.scoreboardPing = "Ping"
+pt.scoreboardBystanderName = "Name als Unschuldiger"
+pt.scoreboardStatus = "Status"
+pt.scoreboardChance = "Wahrscheinlichkeit"
+pt.scoreboardRefresh = "Aktualisieren"
+
+pt.scoreboardJoinTeam = "Beitreten"
+
+pt.scoreboardActionAdmin = "Ist ein Admin"
+pt.scoreboardActionMute = "Stumm schalten"
+pt.scoreboardActionUnmute = "Stumm schalten deaktivieren"
+
+pt.endroundMurdererQuit = "Die Unschuldigen gewinnen! Der Mörder hat sich zurückgezogen"
+pt.endroundBystandersWin = "Die Unschuldigen gewinnen!"
+pt.endroundMurdererWins = "Der Mörder gewinnt!"
+pt.endroundMurdererWas = "Der Mörder war {murderer}"
+
+pt.endroundLootCollected = "Gegenstände eingesammelt"
+
+pt.adminPanel = "Admin-Menü"
+
+pt.spectating = "Zuschauen"
+
+pt.adMelonbomberWhy = "Probier doch mal "
+pt.adMelonbomberBy = " ein Spielmodus von dem Entwickler von Murder!"
+
+pt.voiceHelp = "Hilfe"
+pt.voiceHelpDescription = "Schrei nach Hilfe"
+pt.voiceFunny = "Witz"
+pt.voiceFunnyDescription = "Ein kleiner Scherz"
+pt.voiceScream = "Schrei"
+pt.voiceScreamDescription = "Wie ein kleines Mädchen"
+pt.voiceMorose = "Mürrisch"
+pt.voiceMoroseDescription = "Fühle die Trauer"
+
+
+pt.startHelpBystanderTitle = "Du bist eine unschuldige Person"
+pt.startHelpBystander = {
 	"Es ist ein Mörder auf freiem Fuß",
 	"Lass dich nicht umbringen!"
 }
 
 
-pt.startHelpGunTitle="Du bist eine unschuldige Person"
-pt.startHelpGunSubtitle="mit einer geheimen Waffe"
-pt.startHelpGun={
+pt.startHelpGunTitle = "Du bist eine unschuldige Person"
+pt.startHelpGunSubtitle = "mit einer geheimen Waffe"
+pt.startHelpGun = {
 	"Es ist ein Mörder auf freiem Fuß",
 	"Finde und töte ihn!"
 }
 
-pt.startHelpMurdererTitle="Du bist der Mörder"
-pt.startHelpMurderer={
+pt.startHelpMurdererTitle = "Du bist der Mörder"
+pt.startHelpMurderer = {
 	"Töte jeden",
 	"aber lass dich nicht erwischen!"
 }
 
-pt.murdererFog="Deine dunkle Seite kommt zum Vorschein!"
-pt.murdererFogSub="Töte jemanden, um sie zu verbergen!"
+pt.murdererFog = "Deine dunkle Seite kommt zum Vorschein!"
+pt.murdererFogSub = "Töte jemanden, um sie zu verbergen!"
 
-pt.pressEToDisguiseFor1Loot="[E] verkleiden"
+pt.pressEToDisguiseFor1Loot = "[E] verkleiden"
 
-pt.playerStatusDead="Tot"
+pt.playerStatusDead = "Tot"
